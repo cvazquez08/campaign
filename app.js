c13bf1ca1350f9b0240594991192c7a717657a91
Chris Vazquez <c_vazquez08@yahoo.com>
2020-07-22 16:06:14 -0400

    all

---
+++ app.js
@@ -0,0 +1,29 @@
+$(".variable").slick({
+  dots: true,
+  infinite: true,
+  variableWidth: true,
+});
+
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Mar 9, 2021 00:00:00').getTime(),
+    x = setInterval(function() {    
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'WE WIN;
+      //}
+
+    }, second)
